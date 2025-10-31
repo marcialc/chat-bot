41efabd8692e5add6a193aa079f6ef176a312e02
marcialc <marcialandres06@gmail.com>
2025-10-31 14:09:55 +0100

    fixed cf build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    useLightningcss: false, // Webpack build will use PostCSS instead
+  },
 };
 
 export default nextConfig;
