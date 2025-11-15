33b507319efd38cd88aef1091a80b3d1001db2fe
marcialc <marcialandres06@gmail.com>
2025-11-15 10:44:19 -0500

    update endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/select";
 import { useState } from "react";
 
-const WORKER_URL = "https://shy-water-63ef.marcialandres06.workers.dev";
+const WORKER_URL = "https://mirafind-ai.marcialandres06.workers.dev";
 
 const WORKER_ENDPOINT = {
   chat: `${WORKER_URL}/chat`,
